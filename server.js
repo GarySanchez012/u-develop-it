a249fe08f27ac3bdf7d0f2a985c3fb8834e7896f
arroyhondo <garysanchez012@gmail.com>
2021-08-13 19:48:02 -0400

    added the db queries in the server.js file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //your mysql username
+    user: "root",
+    //your mysql password
+    password: "password",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //default response for any other request (not found)
+//catchall route
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
